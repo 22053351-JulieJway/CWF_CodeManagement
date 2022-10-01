4f955b66db179e592871027ed75460702241e720
22053351-JulieJway <22053351@myrp.edu.sg>
2022-10-01 12:47:29 -0700

    Commited whole project to github

---
+++ src/main/java/com/sddevops/CWF_CodeManagement_Julie/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.CWF_CodeManagement_Julie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
